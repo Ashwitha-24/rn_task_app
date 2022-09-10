91c12781b1b5888e17d744cd7060668c08493245
vardhan <vardhan.miraki@gmail.com>
2022-09-10 15:49:58 +0530

    UI and Functionality of finding fees added

--- App.js
+++ App.js
@@ -8,11 +8,12 @@
 
  import React, {useState, useRef} from 'react';
 import {View} from 'react-native';
+import HomeScreen from './Screens/HomeScreen';
 export default function App() {
  
    return (
     <View>
-
+     <HomeScreen/>
     </View>
  
  
